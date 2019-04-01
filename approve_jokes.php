75db299bf234c2f5be80611f253d7989a4e00b29
Yinbin Zuo <singemazuo@gmail.com>
2019-04-01 11:37:41 -0300

    no message

--- approve_jokes.php
+++ approve_jokes.php
@@ -1,10 +1,10 @@
 <?php
-    include 'includes/session.php';
-    include 'includes/functions.php';
+include 'includes/session.php';
+include 'includes/functions.php';
 
-    $myTitle = 'Approve Jokes';
+$myTitle = 'Approve Jokes';
 
-    // confirm_logged_in_as_admin();
+// confirm_logged_in_as_admin();
 ?>
 
 <!doctype html>
@@ -12,23 +12,23 @@
 
 <?php include 'includes/header.php';?>
 <body>
-    <h1><?php echo $myTitle; ?></h1>
-    <?php
-    include 'includes/open_connection.php';
+<h1><?php echo $myTitle; ?></h1>
+<?php
+include 'includes/open_connection.php';
 
-    $link = make_connection('joker');
+$link = make_connection('joker');
 
-    if ($result = $link->query('CALL getJokesForApproval()')){
-        while ($row = $result->fetch_assoc()){
-            echo "<div class='background'><b>Title: </b>".$row['title']."<br>";
-            echo "<b>Teaser: </b><a href='joke_details.php?id=".$row['id']."'>".$row['teaser']."</a><br>";
-            echo "<b>Joke Text: </b>".$row['joke_text']."<br><br>";
-            echo "<a href='joke_approval.php?id=".$row['id']."'>Approve Joke</a></div><hr>";
-        }
+if ($result = $link->query('CALL getJokesForApproval()')){
+    while ($row = $result->fetch_assoc()){
+        echo "<div class='background'><b>Title: </b>".$row['title']."<br>";
+        echo "<b>Teaser: </b><a href='joke_details.php?id=".$row['id']."'>".$row['teaser']."</a><br>";
+        echo "<b>Joke Text: </b>".$row['joke_text']."<br><br>";
+        echo "<a href='joke_approval.php?id=".$row['id']."'>Approve Joke</a></div><hr>";
     }
-    
-    $link->close();
-    include 'includes/footer.php';
-    ?>
+}
+
+$link->close();
+include 'includes/footer.php';
+?>
 </body>
 </html>
--- includes/footer.php
+++ includes/footer.php
@@ -1,14 +1,14 @@
 <div id="footer">
     <?php
-        if (isset($_SESSION["user_type"])){
-            if ($_SESSION["user_type"] == 'Admin'){
-                include 'includes/admin_footer.php';
-            }else if($_SESSION["user_type"] == 'User'){
-                include 'includes/user_footer.php';
-            }
-        }else{
+    if (isset($_SESSION["user_type"])){
+        if ($_SESSION["user_type"] == 'Admin'){
+            include 'includes/admin_footer.php';
+        }else if($_SESSION["user_type"] == 'User'){
             include 'includes/user_footer.php';
         }
+    }else{
+        include 'includes/user_footer.php';
+    }
     ?>
 </div>
 
